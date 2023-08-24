ae66cdee8d21f8e3a39cdb104933bccf1b189a9e
Bryan Canon <bcanonse@gmail.com>
2023-08-24 08:28:01 -0600

    [ADD]Fluent Api para modelado de tablas

--- Context/TaskContext.cs
+++ Context/TaskContext.cs
@@ -7,5 +7,35 @@ public class TaskContext : DbContext
 
     public TaskContext(DbContextOptions<TaskContext> options) : base(options) { }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(category =>
+        {
+            category.ToTable("categories");
+            category.HasKey(field => field.Id).HasName("pk_category_id");
+            category.Property(field => field.Id).HasColumnName("id");
+            category.Property(field => field.Name).IsRequired().HasMaxLength(150).HasColumnName("name");
+            category.Property(field => field.Description).HasColumnName("description");
+        });
 
+        modelBuilder.Entity<TaskModel>(task =>
+        {
+            task.ToTable("tasks");
+            task.HasKey(field => field.Id).HasName("pk_task_id");
+
+            task.HasOne(field => field.Category)
+                .WithMany(category => category.Tasks)
+                .HasForeignKey(field => field.CategoryId)
+                .HasConstraintName("fk_tasks_category_id");
+
+            task.Property(field => field.Id).HasColumnName("id");
+            task.Property(field => field.CategoryId).HasColumnName("category_id");
+            task.Property(field => field.Title).IsRequired().HasMaxLength(200).HasColumnName("title");
+            task.Property(field => field.Description).HasColumnName("description");
+            task.Property(field => field.Priority).HasColumnName("priority").IsRequired();
+            task.Property(field => field.CreatedDate).HasColumnName("created_date");
+            task.Ignore(field => field.Summary);
+
+        });
+    }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,13 +1,10 @@
 namespace EfPlatzi.Models;
 
-[Table("categories")]
 public class Category
 {
-    [Key]
+    
     public Guid Id { get; set; }
 
-    [Required]
-    [MaxLength(150)]
     public string Name { get; set; }
     public string Description { get; set; }
 
